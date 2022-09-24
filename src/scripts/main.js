bdae6207bd62338458bb00180f134cd3bb8cf7e4
Sayazhan Onlasyn <sayazhan.onlassyn@mail.ru>
2022-09-24 17:05:33 +0600

    feat: build trees on foundation

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,7 +2,7 @@ import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 const FOUNDATION_HEIGHT = 0.2;
-const FLOOR_HEIGTH = 0.8;
+const FLOOR_HEIGTH = 1.8;
 const ODD_FLOORS_COLOR = 0xc4989a;
 const EVEN_FLOORS_COLOR = 0x9a8c8c;
 
@@ -37,6 +37,17 @@ function main() {
       ODD_FLOORS_COLOR
     )
   );
+
+  buildTree(scene, 10, -10);
+  buildTree(scene, 10, -8);
+  buildTree(scene, -5, -8);
+  buildTree(scene, -10, -10);
+  buildTree(scene, -10, -5);
+  buildTree(scene, 7, 9);
+  buildTree(scene, 8, -5);
+  buildTree(scene, -10, 12);
+  buildTree(scene, -10, 8);
+  buildTree(scene, -4, 12);
 
   renderer.setAnimationLoop(() => animate(renderer, scene, camera, orbit));
 
@@ -83,7 +94,7 @@ function buildLights(scene) {
 }
 
 function buildFoundation(scene) {
-  const boxGeometry = new THREE.BoxGeometry(14, FOUNDATION_HEIGHT, 14);
+  const boxGeometry = new THREE.BoxGeometry(30, FOUNDATION_HEIGHT, 30);
   const boxMaterial = new THREE.MeshStandardMaterial({ color: 0xdeb775 });
   const box = new THREE.Mesh(boxGeometry, boxMaterial);
 
@@ -95,7 +106,7 @@ function buildFoundation(scene) {
 }
 
 function buildFloor(scene, baseY = FOUNDATION_HEIGHT / 2, color) {
-  const boxGeometry = new THREE.BoxGeometry(6, FLOOR_HEIGTH, 6);
+  const boxGeometry = new THREE.BoxGeometry(8, FLOOR_HEIGTH, 8);
   const boxMaterial = new THREE.MeshStandardMaterial({ color });
   const box = new THREE.Mesh(boxGeometry, boxMaterial);
 
@@ -142,4 +153,56 @@ function listenToFloorsChange(scene, floors) {
     const numberOfFloorsLabel = document.getElementById("number-of-floors");
     numberOfFloorsLabel.innerText = value;
   }
+}
+
+function buildTree(scene, positionX, positionZ) {
+  geometry = new THREE.BoxGeometry(1, 1, 1);
+
+  var leaveDarkMaterial = new THREE.MeshLambertMaterial({ color: 0x91e56e });
+  var leaveLightMaterial = new THREE.MeshLambertMaterial({ color: 0xa2ff7a });
+  var leaveDarkDarkMaterial = new THREE.MeshLambertMaterial({
+    color: 0x71b356,
+  });
+  var stemMaterial = new THREE.MeshLambertMaterial({ color: 0x7d5a4f });
+
+  var stem = new THREE.Mesh(geometry, stemMaterial);
+  stem.position.set(0, 0, 0);
+  stem.scale.set(0.3, 1.5, 0.3);
+
+  var squareLeave01 = new THREE.Mesh(geometry, leaveDarkMaterial);
+  squareLeave01.position.set(0.5, 1.6, 0.5);
+  squareLeave01.scale.set(0.8, 0.8, 0.8);
+
+  var squareLeave02 = new THREE.Mesh(geometry, leaveDarkMaterial);
+  squareLeave02.position.set(-0.4, 1.3, -0.4);
+  squareLeave02.scale.set(0.7, 0.7, 0.7);
+
+  var squareLeave03 = new THREE.Mesh(geometry, leaveDarkMaterial);
+  squareLeave03.position.set(0.4, 1.7, -0.5);
+  squareLeave03.scale.set(0.7, 0.7, 0.7);
+
+  var leaveDark = new THREE.Mesh(geometry, leaveDarkMaterial);
+  leaveDark.position.set(0, 1.2, 0);
+  leaveDark.scale.set(1, 2, 1);
+
+  var leaveLight = new THREE.Mesh(geometry, leaveLightMaterial);
+  leaveLight.position.set(0, 1.2, 0);
+  leaveLight.scale.set(1.1, 0.5, 1.1);
+
+  var ground = new THREE.Mesh(geometry, leaveDarkDarkMaterial);
+  ground.position.set(0, -1, 0);
+  ground.scale.set(2.4, 0.8, 2.4);
+
+  let tree = new THREE.Group();
+  tree.add(leaveDark);
+  tree.add(leaveLight);
+  tree.add(squareLeave01);
+  tree.add(squareLeave02);
+  tree.add(squareLeave03);
+  tree.add(ground);
+  tree.add(stem);
+
+  tree.position.set(positionX, 1.6, positionZ);
+
+  scene.add(tree);
 }
