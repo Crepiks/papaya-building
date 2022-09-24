0a31ef39bf10a64b5d98d53718766f93adaa26ec
Sayazhan Onlasyn <sayazhan.onlassyn@mail.ru>
2022-09-24 12:29:02 +0600

    feat: create basic moving scene

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,36 @@
-console.log("Hello");
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+
+const renderer = new THREE.WebGLRenderer();
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
+const orbit = new OrbitControls(camera, renderer.domElement);
+
+const axesHelper = new THREE.AxesHelper(3);
+scene.add(axesHelper);
+
+camera.position.set(5, 5, 5);
+orbit.update();
+
+const boxGeometry = new THREE.BoxGeometry();
+const boxMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const box = new THREE.Mesh(boxGeometry, boxMaterial);
+scene.add(box);
+
+function animate() {
+  orbit.update();
+
+  renderer.render(scene, camera);
+}
+
+renderer.setAnimationLoop(animate);
