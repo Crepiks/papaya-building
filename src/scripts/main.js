123d12bd1c75d6cd0e7fba5c1e6c5a8792bc25b0
Sayazhan Onlasyn <sayazhan.onlassyn@mail.ru>
2022-09-24 17:25:48 +0600

    feat: build and add car to foundation

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -49,6 +49,8 @@ function main() {
   buildTree(scene, -10, 8);
   buildTree(scene, -4, 12);
 
+  buildCar(scene);
+
   renderer.setAnimationLoop(() => animate(renderer, scene, camera, orbit));
 
   listenToFloorsChange(scene, floors);
@@ -202,7 +204,94 @@ function buildTree(scene, positionX, positionZ) {
   tree.add(ground);
   tree.add(stem);
 
-  tree.position.set(positionX, 1.6, positionZ);
+  tree.position.set(positionX, 1.4, positionZ);
 
   scene.add(tree);
+}
+
+function buildCar(scene) {
+  const car = new THREE.Group();
+
+  const backWheel = createWheels();
+  backWheel.position.y = 6;
+  backWheel.position.x = -18;
+  car.add(backWheel);
+
+  const frontWheel = createWheels();
+  frontWheel.position.y = 6;
+  frontWheel.position.x = 18;
+  car.add(frontWheel);
+
+  const main = new THREE.Mesh(
+    new THREE.BoxBufferGeometry(60, 15, 30),
+    new THREE.MeshLambertMaterial({ color: 0xa52523 })
+  );
+  main.position.y = 12;
+  car.add(main);
+
+  const carFrontTexture = getCarFrontTexture();
+
+  const carBackTexture = getCarFrontTexture();
+
+  const carRightSideTexture = getCarSideTexture();
+
+  const carLeftSideTexture = getCarSideTexture();
+  carLeftSideTexture.center = new THREE.Vector2(0.5, 0.5);
+  carLeftSideTexture.rotation = Math.PI;
+  carLeftSideTexture.flipY = false;
+
+  const cabin = new THREE.Mesh(new THREE.BoxBufferGeometry(33, 12, 24), [
+    new THREE.MeshLambertMaterial({ map: carFrontTexture }),
+    new THREE.MeshLambertMaterial({ map: carBackTexture }),
+    new THREE.MeshLambertMaterial({ color: 0xffffff }),
+    new THREE.MeshLambertMaterial({ color: 0xffffff }),
+    new THREE.MeshLambertMaterial({ map: carRightSideTexture }),
+    new THREE.MeshLambertMaterial({ map: carLeftSideTexture }),
+  ]);
+  cabin.position.x = -6;
+  cabin.position.y = 25.5;
+  car.add(cabin);
+
+  car.scale.set(0.03, 0.03, 0.03);
+  car.position.set(10, 0.1, 10);
+
+  scene.add(car);
+}
+
+function createWheels() {
+  const geometry = new THREE.BoxBufferGeometry(12, 12, 33);
+  const material = new THREE.MeshLambertMaterial({ color: 0x333333 });
+  const wheel = new THREE.Mesh(geometry, material);
+  return wheel;
+}
+
+function getCarFrontTexture() {
+  const canvas = document.createElement("canvas");
+  canvas.width = 64;
+  canvas.height = 32;
+  const context = canvas.getContext("2d");
+
+  context.fillStyle = "#ffffff";
+  context.fillRect(0, 0, 64, 32);
+
+  context.fillStyle = "#666666";
+  context.fillRect(8, 8, 48, 24);
+
+  return new THREE.CanvasTexture(canvas);
+}
+
+function getCarSideTexture() {
+  const canvas = document.createElement("canvas");
+  canvas.width = 128;
+  canvas.height = 32;
+  const context = canvas.getContext("2d");
+
+  context.fillStyle = "#ffffff";
+  context.fillRect(0, 0, 128, 32);
+
+  context.fillStyle = "#666666";
+  context.fillRect(10, 8, 38, 24);
+  context.fillRect(58, 8, 60, 24);
+
+  return new THREE.CanvasTexture(canvas);
 }
