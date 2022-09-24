258bbdc882366749dfda0318d7e98dfbd8afab9e
Sayazhan Onlasyn <sayazhan.onlassyn@mail.ru>
2022-09-24 12:42:19 +0600

    feat: create foundation block

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,5 @@
 import * as THREE from "three";
+import { Color } from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 const renderer = new THREE.WebGLRenderer();
@@ -7,23 +8,24 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 const scene = new THREE.Scene();
+scene.background = new THREE.Color(0x446481);
+
 const camera = new THREE.PerspectiveCamera(
   75,
   window.innerWidth / window.innerHeight,
   0.1,
   1000
 );
+camera.position.set(10, 10, 10);
 
 const orbit = new OrbitControls(camera, renderer.domElement);
+orbit.update();
 
 const axesHelper = new THREE.AxesHelper(3);
 scene.add(axesHelper);
 
-camera.position.set(5, 5, 5);
-orbit.update();
-
-const boxGeometry = new THREE.BoxGeometry();
-const boxMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const boxGeometry = new THREE.BoxGeometry(10, 0.2, 10);
+const boxMaterial = new THREE.MeshBasicMaterial({ color: 0xdeb775 });
 const box = new THREE.Mesh(boxGeometry, boxMaterial);
 scene.add(box);
 
